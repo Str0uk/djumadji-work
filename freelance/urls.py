9ef35e6a601a87b8884653a480e6f5e6b4711abc
Misha <strouk7@yandex.ru>
2021-08-31 14:10:38 +0300

    new

--- freelance/urls.py
+++ freelance/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+from django.views.static import serve
+
 from vacancies.views import *
 from django.conf import settings
 from django.conf.urls.static import static
@@ -31,5 +33,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
-urlpatterns += static(settings.MEDIA_URL,
-                      document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT,}),]
